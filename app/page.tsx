adb84a97823780d182ce6005ad92e322c6e67850
TaskManagerOL <1975771886@qq.com>
2025-04-06 15:16:19 +0800

    feat:init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,128 @@
-import Image from "next/image";
+
+"use client";  
+import React, { useState, useEffect } from 'react';
+
+const IndexBox = () =>{
+  const [visibleBoxes, setVisibleBoxes] = useState([]);
+  const [visibleMainBox, setvisibleMianBox] = useState(false)
+  const boxWidthArr = Array.from({ length: 10 }, (_, index) => index / 2 + 0.5);
+  useEffect(() => {
+    const promise = new Promise<void>((resolve) => {
+      boxWidthArr.forEach((_, index) => {
+        setTimeout(() => {
+          setVisibleBoxes((prev) => [...prev, index]); 
+          if(index === boxWidthArr.length-1)setTimeout(()=>resolve(),50)
+        }, index * 150); 
+      })  
+    }) 
+    promise.then(()=>{
+      setvisibleMianBox(true)
+    })
+  }, [boxWidthArr]);
+  return (
+    <div className="w-4/5 h-screen min-h-[600px] flex items-center">
+      {boxWidthArr.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              width: `${item}%`,
+              height: `${item * 14 + 5}%`,
+              opacity: visibleBoxes.includes(index) ? 1 : 0,
+              transition: 'opacity 0.5s ease',
+              background: `rgb(${-item*20+150},${-item*20+150},${-item*20+150})`,
+              margin: `0 ${item+2}px 0 0`
+            }}
+            className="rounded-l-3xl"
+          ></div>
+      ))}
+      {
+        <div 
+          className="w-2/3 h-4/5 bg-[#303030] rounded-3xl flex flex-col justify-around items-center"
+          style={{
+            opacity:visibleMainBox?1:0,
+            transition: 'opacity 0.5s ease'
+          }}
+        >   
+
+          <div 
+            className='text-[#eee] text-[clamp(8px,7vw,120px)] select-none flex flex-col justify-center mt-[15vh]'
+            style={{
+              fontFamily:'SuperLobster',
+            }}
+          >
+            <div className='flex'>Hi <div className='hover:rotate-60 transition'>👋</div> Here,</div>
+            <div>TaskManagerOL.</div>
+          </div>
+          <div
+            className='text-[#eee] flex select-none flex-wrap'
+            style={{
+              fontFamily:'Backso'
+            }}
+          >
+            <div className='mx-2.5'>Gentler</div> / <div className='mx-2.5'>Faster</div> / <div className='mx-2.5'>More passionate</div>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
+
+const BlogBox = () => {
+  return (
+    <div 
+      className="w-4/5 my-15 bg-[#a9a9a9] relative flex flex-wrap"
+    >
+      <div 
+        className='absolute top-[-6rem] left-2 text-[100px] font-bold'
+        style={{
+          fontFamily:'KGPerfectPenmanship'
+        }}
+      >
+        Blog.
+      </div>
+      <div
+        className='w-1/2 min-w-[300px] h-[600px] bg-[#616161]'
+      >
+
+      </div>
+      <div
+        className='w-1/2 min-w-[300px] h-[600px] bg-[#616161]'
+      >
+
+      </div>
+    </div>
+  )
+}
+
+const ProjectBox = () => {
+  return (
+    <div 
+      className="w-4/5 h-[700px] my-15 bg-[#a9a9a9] relative flex flex-wrap"
+    >
+      <div 
+        className='absolute top-[-6rem] left-2 text-[100px] font-bold'
+        style={{
+          fontFamily:'KGPerfectPenmanship'
+        }}
+      >
+        Project.
+      </div>
+      <div
+        className='w-1/2 min-w-[300px] bg-[#616161]'
+      >
+
+      </div>
+    </div>
+  )
+}
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="w-full h-auto flex justify-center items-center bg-[#fefbf5] flex-col">
+      <IndexBox/>
+      <BlogBox/>
+      <ProjectBox/>
     </div>
   );
 }
