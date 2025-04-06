76f51601a874f44c3f0b49d039e05e02c9a99457
TaskManagerOL <1975771886@qq.com>
2025-04-06 14:25:01 +0800

    config:export config

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,8 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
 };
 
+module.exports = {
+  exportTrailingSlash: true,  // 确保生成的 URL 以斜杠结尾（适用于 GitHub Pages）
+};
+
 export default nextConfig;
