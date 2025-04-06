eb466bdfb0396157a3d25e64371d92492f4503c0
TaskManagerOL <1975771886@qq.com>
2025-04-06 15:07:35 +0800

    config:add folder

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: true,
-};
-
-module.exports = {
-  exportTrailingSlash: true,  // 确保生成的 URL 以斜杠结尾（适用于 GitHub Pages）
+  output: 'export',
+  trailingSlash: true,  // 为了兼容 GitHub Pages
 };
 
 export default nextConfig;
