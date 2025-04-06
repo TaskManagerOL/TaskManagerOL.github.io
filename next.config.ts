f5aefafb2b5a8b6bfe4617a6185648618808c7ef
TaskManagerOL <1975771886@qq.com>
2025-04-06 17:56:32 +0800

    config:address err

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,  // 为了兼容 GitHub Pages
+  reactStrictMode: true,
+  basePath: '/out', // 设置应用的基础路径
+  assetPrefix: '/out/'
 };
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},  // 引用 tailwindcss 插件
+    autoprefixer: {},  // 引用 autoprefixer 插件
+  },
 };
 
 export default config;
