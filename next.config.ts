44e516289c4e07d0bdb31b1bf2fa828a201ad69d
TaskManagerOL <1975771886@qq.com>
2025-04-06 20:02:56 +0800

    ignore

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,  // 为了兼容 GitHub Pages
   reactStrictMode: true,
-  basePath: '/out', // 设置应用的基础路径
-  assetPrefix: '/out/'
+  basePath: '/dist',
+  assetPrefix: '.',
+  distDir: 'dist',
 };
 
 export default nextConfig;
